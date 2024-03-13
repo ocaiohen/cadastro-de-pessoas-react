6a1d7226094edfcd2d02f3b1da299a6e9702b013
ocaiohen <caiohencontato@gmail.com>
2024-03-12 21:40:36 -0300

    Conexão com backend. Uso do UseEffect no React.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react"
+import { v4 as uuidv4 } from 'uuid'
+import axios from "axios"
 import 
 {
   MainContainer, 
@@ -13,20 +15,40 @@ import
   ButtonTrashCan
 } 
 from "./Styles.js";
-import { v4 as uuidv4 } from 'uuid'
+
 const App = () => {
   const [users, setUsers] = useState([])
   const inputName = useRef()
   const inputAge = useRef()
 
-  const addNewUser = () => {
+   const addNewUser = async () => {
     if(inputName.current.value && inputAge.current.value){
-      setUsers([...users, {id : uuidv4(), name: inputName.current.value, age: inputAge.current.value}])
+      const id = uuidv4()
+      const age = inputAge.current.value
+      const name = inputName.current.value
+      await axios.post("http://localhost:3001/documents", {
+       id: id,
+       name: name,
+       age: age
+      })
+      // console.log(response.data)
+      // console.log(await axios.get("http://localhost:3001/documents"))
+      const {data: newUsers} = await axios.get("http://localhost:3001/documents")
+      setUsers(newUsers)
     }
     else{
       alert("Insira os valores nos dois inputs.")
     }
   }
+
+  useEffect(()=>{
+    const getAndSetUsers = async () => {
+      const {data: newUsers} = await axios.get("http://localhost:3001/documents")
+      setUsers(newUsers)
+    }
+    getAndSetUsers()
+  },[])
+
   const deleteUser = (id) =>{
     const newUsers = users.filter((user) => user.id !== id)
     setUsers(newUsers)
